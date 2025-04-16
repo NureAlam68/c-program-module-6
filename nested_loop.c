5475c434447005d4aca89bee803b87d8bcd859d4
NureAlam68 <nurealam151068@gmail.com>
2025-04-16 11:49:43 +0600

    nested loop

---
+++ nested_loop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for(int i = 1; i <= 10; i++)
+    {
+        for(int x = 1; x <= 5; x++)
+        {
+            printf("%d ", x);
+        }
+        printf("\n");
+    }
+    return 0;
+}
